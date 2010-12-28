a11fe11fd422acb440fd305282e0e45257a11c5e
Aaron Batalion <aaron@hungrymachine.com>
2010-12-28 14:09:33 -0500

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
   config.time_zone = 'UTC'
-  config.action_controller.session = { :key => "_myapp_session", :secret => "some secret phrase" }
+  config.action_controller.session = { :key => "_myapp_session", :secret => "5c69035af46b1e9543d02ffffb2b31324242342342342424244" }
 end
